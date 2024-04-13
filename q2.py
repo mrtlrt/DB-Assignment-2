25ed5d2889ccf74a37773215df929fbeda75512d
Siddharth Ganesh <sid@Sids-MacBook-Pro.local>
2024-04-14 01:24:13 +0800

    hopefully final edits part 2

--- q2.py
+++ q2.py
@@ -21,34 +21,36 @@ df = (
     .csv("hdfs://%s:9000/assignment2/part1/input/" % (hdfs_nn))
 )
 
-df = df.na.drop(how="any", subset=["Price Range", "Rating"])
+df = df.filter(df["Price Range"].isNotNull())
 
 # finding the best restaurant s for each city for each price range (in terms of rating).
 
-best_restaurants_df = (df.groupBy(["Price Range", "City"]).agg(max("Rating")).withColumn("Rating", col("max(Rating)")).drop("max(Rating)"))
+best_restaurants_df = (df.groupBy("City", "Price Range").agg(max(col("Rating")).alias("Rating")))
+
 
 # finding the worst restaurant s for each city for each price range (in terms of rating).
 
-worst_restaurants_df = (df.groupBy(["Price Range", "City"]).agg(min("Rating")).withColumn("Rating", col("min(Rating)")).drop("min(Rating)"))
+worst_restaurants_df = df.groupBy("City", "Price Range").agg(min(col("Rating")).alias("Rating"))
+
 
 merged_df = best_restaurants_df.union(worst_restaurants_df)
 
 final_df = merged_df.join(df, on=["Price Range", "City", "Rating"], how="inner")
 
-final_df = (final_df.dropDuplicates(["Price Range", "City", "Rating"])
-    .select(
-        "_c0",
-        "Name",
-        "City",
-        "Cuisine Style",
-        "Ranking",
-        "Rating",
-        "Price Range",
-        "Number of Reviews",
-        "Reviews",
-        "URL_TA",
-        "ID_TA",
-    ).sort(col("City").asc(), col("Price Range").asc(), col("Rating").desc()))
+final_df = final_df.dropDuplicates(["Price Range", "City", "Rating"]).orderBy(col("City").asc(),col("Rating").desc())
+
+final_df = final_df.select("_c0",
+            "Name",
+            "City",
+            "Cuisine Style",
+            "Ranking",
+            "Rating",
+            "Price Range",
+            "Number of Reviews",
+            "Reviews",
+            "URL_TA",
+            "ID_TA"
+)
 
 final_df.show()
 
